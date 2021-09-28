d4e07dbe45dc43bd764d7bdb5b70c9493e9f029c
MixDevanim <91450633+MixDevanim@users.noreply.github.com>
2021-09-28 10:06:45 +0300

    Add files via upload

--- mainloop.js
+++ mainloop.js
@@ -37,6 +37,7 @@ function main() {
             fpsTimer = 0.0;
             
             fpsElement.textContent = ""+(1.0/maxDelta).toFixed(2);
+            console.log(""+(1.0/maxDelta).toFixed(2));
             
             maxDelta = 1e-5;
         }
